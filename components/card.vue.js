a178aaf63d377fcdef5714822bc7e987e43b0bb5
Jean-Francis Bastien <bhacaz@gmail.com>
2021-10-19 07:40:05 -0400

    use flex row for now

--- components/card.vue.js
+++ components/card.vue.js
@@ -8,15 +8,13 @@ const Card = {
           <div class="media-content">
             <p class="title is-4">{{ participant.title }}</p>
             <p class="subtitle is-5">{{ participant.name }}</p>
-            <p class="subtitle is-5">{{ participant.formation }}</p>
+            <p class="subtitle is-6">{{ participant.formation }}</p>
           </div>
         </div>
 
         <div class="content">
-          <p>{{ participant.description }}</p>
-          <div class="content">
-            <span class="tag is-info">{{ participant.period }}</span>
-          </div>
+          <p>{{ participant.description }}...</p>
+          <span class="tag is-info">{{ participant.period }}</span>
         </div>
       </div>
     </div>
