52ab7f833bbb381540c53b188328111005106ef0
Jean-Francis Bastien <bhacaz@gmail.com>
2021-10-18 20:35:31 -0400

    CSV transformed

---
+++ components/card.vue.js
@@ -0,0 +1,24 @@
+
+const Card = {
+    props: ['participant'],
+    template: `
+    <div class="card card-width">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ participant.title }}</p>
+            <p class="subtitle is-5">{{ participant.name }}</p>
+            <p class="subtitle is-5">{{ participant.formation }}</p>
+          </div>
+        </div>
+
+        <div class="content">
+          <p>{{ participant.description }}</p>
+          <div class="content">
+            <span class="tag is-info">{{ participant.period }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+}
---
+++ scripts/csvJSON.js
@@ -0,0 +1,27 @@
+function csvJSON(csv){
+    var lines=csv.split("\n").map(line => {
+        line = line.substring(1);
+        return line.slice(0,-1)
+    })
+    var result = [];
+
+    // NOTE: If your columns contain commas in their values, you'll need
+    // to deal with those before doing the next step
+    // (you might convert them to &&& or something, then covert them back later)
+    // jsfiddle showing the issue https://jsfiddle.net/
+    var headers=lines[0].split("\",\"");
+
+    for(var i=1;i<lines.length;i++){
+
+        var obj = {};
+        var currentline=lines[i].split("\",\"");
+
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = currentline[j];
+        }
+
+        result.push(obj);
+
+    }
+    return result;
+}
