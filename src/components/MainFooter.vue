4e8e9097f42b3e3be53ef1fa8472919e330e6c74
Jean-Francis Bastien <jfbastien@petalmd.com>
2022-10-27 20:16:51 -0400

    Get first hours

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        <strong>Formulaire d'inscription:</strong>
+        <strong>Formulaire d'inscription:  </strong>
         <a href="https://forms.gle/7e1P9vDcW1Ydvh4k7">Google Forms</a><br />
         Développé par
         <a href="https://github.com/Bhacaz">Jean-Francis Bastien</a>.<br />
