96ab284b9a579bedd9c64e7333b34a4c2fc147de
Jean-Francis Bastien <jfbastien@petalmd.com>
2022-10-27 20:03:27 -0400

    Convert csv

---
+++ src/scripts/csv_to_json.rb
@@ -0,0 +1,22 @@
+require 'json'
+require 'csv'
+
+participants = []
+
+CSV.open('./data/data.csv', 'r', headers: true) do |csv|
+  csv.each do |row|
+    participants << {
+      name: row['Prénom'] + " " + row['Nom'],
+      title: row["Quel est le titre de votre emploi ?"],
+      domain: row['Métier'] || '',
+      formation: row["Le niveau d'études pour occuper cet emploi habituellement."],
+      description: row[ "Afin d'aider les élèves à comprendre et à s'intéresser à votre emploi, décrivez-nous brièvement ce que vous faites. Ce texte servira aux élèves lorsqu'ils devront choisir leurs conférences."],
+      period: row["À quel(s) moment(s) seriez-vous disponible pour animer votre atelier/conférence le 22 novembre ? (atelier de 45 minutes) *Vous pouvez cocher plusieurs cases"],
+      link_reperes: row["Lien repères"],
+      holland_codes: [row["CODE HOLLAND 1"], row["CODE HOLLAND 2"], row["CODE HOLLAND 3"]],
+    }
+  end
+end
+
+json = JSON.pretty_generate(participants)
+File.open('./src/models/participants.json', 'w') { |file| file.write(json) }
